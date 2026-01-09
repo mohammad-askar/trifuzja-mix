697c2e35965f64f58bf9084bad7bf453cc0bb91b
askar-mizo <104626587+askar-mizo@users.noreply.github.com>
2026-01-09 03:29:12 +0100

    changing next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,25 +1,26 @@
 // types/next-auth.d.ts
 import type { DefaultSession } from "next-auth";
 
+/** النظام Admin فقط */
 export type Role = "admin";
 
 declare module "next-auth" {
   interface Session {
     user: DefaultSession["user"] & {
       id: string;
-      role: Role; // ✅ غير اختياري
+      role?: Role; // ✅ لازم تبقى اختيارية لتفادي كسر AdapterUser
     };
   }
 
   interface User {
     id: string;
-    role: Role; // ✅ غير اختياري
+    role?: Role; // ✅ اختيارية
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
     id?: string;
-    role: Role; // ✅ غير اختياري
+    role?: Role; // ✅ اختيارية
   }
 }
